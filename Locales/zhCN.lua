0b322c8189a4ed2e009e5f66b87f605fec157dcd
enderneko <enderneko-dev@outlook.com>
2025-04-28 16:47:49 +0800

    Update zhCN

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -3,3 +3,34 @@ local L = select( 2, ...).L
 
 L["BFBlackMarket"] = "大脚黑市"
 L["BFBM"] = "大脚黑市"
+L["New version (%s) available! Please consider updating."] = "发现新版本(%s)，建议您更新。"
+
+L["CURRENT"] = "当前商品"
+L["HISTORY"] = "历史记录"
+L["Config"] = "设置"
+
+-- current
+L["Current Server"] = "当前服务器"
+L["Last Update"] = "最后更新"
+L["Only update time when data changes"] = "仅在数据变化时更新时间"
+L["No data"] = "没有数据"
+L["Servers"] = "服务器"
+
+-- history
+L["Avg"] = "均价"
+L["Add Item to Watchlist"] = "添加物品到关注列表"
+L["Item ID"] = "物品ID"
+L["No items found"] = "没有找到物品"
+L["items"] = "物品"
+
+-- config
+L["Hold Ctrl to show item tooltips"] = "按住Ctrl键显示物品鼠标提示"
+L["No data receiving in instances"] = "在副本时不接收数据"
+L["Price change alerts"] = "价格变化提醒"
+L["Show notification popups when watched items change price"] = "当关注的物品价格变化时显示通知弹窗"
+
+-- detail
+L["Available on %s servers"] = "正在 %s 个服务器上拍卖"
+
+-- popup
+L["%s is on the Black Market!\nCurrent bid: %s\nServer: %s"] = "%s 在黑市上架了！\n当前竞标价：%s\n服务器：%s"
